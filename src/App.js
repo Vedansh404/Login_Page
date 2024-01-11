99da19c9615273e23cd5d7656792f67fc7b5f9b8
Vedansh Rashinkar <vedanshrashinkar77@gmail.com>
2024-01-11 15:11:19 +0530

    Files Uploaded

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import LoginForm from './Components/LoginForm/LoginForm';
+import './App.css';
+import { Component } from 'react';
+
+
+function App() {
+  return (
+    <div className="App" style={{
+      backgroundImage: ``
+    }}>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
